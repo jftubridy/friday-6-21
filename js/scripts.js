69ffa53df094ac03aa5cc742aa00a5c3ef38c4d2
Joseph Tubridy <jftubridy@gmail.com>
2019-06-21 15:02:31 -0700

    I know it doesn't looks like I'm doing much, but i'm trying to get it working and am confusing myself

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-// Business Logic for Pizza Palace ---------
+//Business Logic for Pizza Palace ---------
 function PizzaPalace() {
   this.pizzas = [],
   this.currentId = 0
+}
 //pushes pizzas with id to the PizzaPalace
 PizzaPalace.prototype.myPizza = function(pizza) {
   pizza.id = this.assignId();
@@ -37,10 +38,12 @@ PizzaPalace.prototype.deletePizza = function(id) {
   return false;
 }
 
+
+
 // Business Logic for Pizza ---------
 function Pizza(size, toppings, cost) {
-  this.size = size,
-  this.toppings = [],
+  this.size = size;
+  this.toppings = [];
   // this.cost = size + toppings.length,
 }
 
@@ -58,104 +61,57 @@ function displayPizza(pizzaToDisplay) {
   var pizzasList = $("ul#pizzas");
   var htmlForPizzaInfo = "";
   pizzaToDisplay.pizzass.forEach(function(pizza) {
-    htmlForPizzaInfo += "<li id=" + contact.id + ">" + pizza.firstName + " " + contact.lastName + "</li>";
+    htmlForPizzaInfo += "<li id=" + pizza.id + ">" + pizza.size + " " + pizza.toppings + "</li>";
   });
-  contactsList.html(htmlForContactInfo);
+  pizzasList.html(htmlForPizzaInfo);
 }
 // shows contact info  and adds button for delete
-function showPizza(contactId) {
-  var contact = addressBook.findPizza(contactId);
-  $('#show-contact').show();
-  $(".first-name").html(pizza.firstName);
+function showPizza(pizzaId) {
+  var pizza = pizzaPalace.findPizza(pizzaId);
+  $('#show-pizza').show();
+  $(".size").html(pizza.size);
 
   var buttons = $("#buttons");
   buttons.empty();
-  buttons.append("<button class ='deleteButton' id =" + + contact.id + ">Delete</button>");
+  buttons.append("<button class ='deleteButton' id =" + pizza.id + ">Delete</button>");
 }
 // listens for action taken
 function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
-  showContact(this.id);
+  $("ul#pizzas").on("click", "li", function() {
+  showPizza(this.id);
   });
   $("#buttons").on("click", ".deleteButton", function(){
-    addressBook.deleteContact(this.id);
+    pizzaPalace.deletePizza(this.id);
     $("#show-contact").hide();
-    displayPizza(addressBook);
+    displayPizza(pizzaPalace);
   });
 };
 
 $(document).ready(function() {
   attachContactListeners();
-  $("form#new-contact").submit(function(event) {
+  $("form#newPizza").submit(function(event) {
     event.preventDefault();
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedPhoneNumber = $("input#new-phone-number").val();
-    // var inputtedEmail =
-    // $('input#email').val();
-    // var inputtedHomeAddress = $('input#homeAddress').val();
-    $('input#Address').val();
-    var inputtedFirstAddressType = $('input#firstAddress').val();
-    var inputtedSecondAddress = $('input#secondAddress').val();
-    var inputtedThirdAddress = $('input#thirdAddress').val();
-    var inputtedFourthAddress = $('input#fourthAddress').val();
-    //pulled class id and plugged into output
-    var firstType = $('#firstType').val();
-    var secondType = $('#secondType').val();
-    var thirdType = $('#thirdType').val();
-    var fourthType = $('#fourthType').val();
-
-
-    var address= new Address(inputtedFirstAddressType, inputtedSecondAddress, inputtedThirdAddress, inputtedFourthAddress);
-    console.log(address);
-    $("#address").append("<li>"+ firstType +" "+ address.firstAddress+ "<br>" +secondType +" "+address.secondAddress+ "<br>" +thirdType +" "+address.thirdAddress+ "<br>" +fourthType +" "+address.fourthAddress+ "</li>")
+    var inputtedSize = $("input#size").val();
+    var inputtedToppings = $("input.topChecks").val();
+    console.log(pizza);
+    var pizza= new Pizza(inputtedSize, inputtedToppings);
+    $("#show-pizza").append("<li>"+ size +" "+ "<br>" + "</li>");
 
   // clears user-input fields
   $("form")[0].reset();
-   // $("input#new-first-name").val("");
-   // $("input#new-last-name").val("");
-   // $("input#new-phone-number").val("");
-   // $('input#email').val("");
-   // $('input#homeAddress').val("");
-   // $('input#firstAddressn').val("");
-   // $('input#secondAddressn').val("");
-   // $('input#thirdAddressn').val("");
-   // $('input#fourthAddressn').val("");
+
+    var newPizza = new Pizza(inputtedSize, inputtedToppings);
+      pizzaPalace.addPizza(newPizza);
+    displayPizza(pizzaPalace);
+  });
+});
 
 
-
-
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, Address);
-    // var newAddress = new Address(inputtedFirstAddressType, inputtedSecondAddress, inputtedThirdAddress, inputtedFourthAddress)
-    addressBook.addContact(newContact);
-    displayPizza(addressBook);
-  })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//User Logic
-$();
-var size = $("#size").val();
-
-var flavor = $("input:radio[name=top]:checked").val();
+//
+//
+// //User Logic
+// $();
+// var size = $("#size").val();
+//
+// var flavor = $("input:checkbox[name=top]:checked").val();
+// });
