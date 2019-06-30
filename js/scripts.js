1743788cf4f0e6d30b4c02c27af055124d66c2b9
Joseph Tubridy <jftubridy@gmail.com>
2019-06-29 18:49:31 -0700

    no longer undefined, but nothing is going into toppings array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic for Pizza ---------
 function Pizza(size, toppings, cost) {
   this.size = size,
-  this.toppings = []
+  this.toppings = [],
   this.cost = cost;
   // this.cost = this.size + this.toppings.length
 }
@@ -17,20 +17,23 @@ $(document).ready(function() {
   $("form#newPizza").submit(function(event) {
     event.preventDefault();
     var inputtedSize = parseInt($("#size").val());
-    alert(inputtedSize);
+    console.log(inputtedSize);
+    debugger;
     var inputtedToppings = [];
-    $("input:checkbox[name=top]:checked")each(function(){
+    function numTop() {
+      inputtedToppings.push("input:checkbox[name=top]:checked").value();
+    // }$("input:checkbox[name=top]:checked")each(function(){
       var topping = $(this).val();
       inputtedToppings.push(topping);
 //going back and checking the transportation assignment to try and get checkboxes to work
-    alert(inputtedToppings);
+    console.log(numTop);
 
     var customPizza= new Pizza(inputtedSize, inputtedToppings);
-    console.log(customPizza.cost);
+    console.log(customPizza.size);
     $("#show-pizza").append("<li>"+ customPizza +" "+ "<br>" + "</li>");
     alert(toppings.length);
   // clears user-input fields
   $("form")[0].reset();
-  });
+  };
 });
 });
